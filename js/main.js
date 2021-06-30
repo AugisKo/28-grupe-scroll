c1b7650cd9bb6e0eccabcb6ea11aca6505e5fbd7
Rimantas <belauzas@yahoo.com>
2021-06-30 18:39:03 +0300

    Back to top scroll event

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log('arfdhg');
+const backToTopDOM = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 500) {
+        backToTopDOM.classList.remove('hide');
+    } else {
+        backToTopDOM.classList.add('hide');
+    }
+})
