63f5ef219db7c3d5b7869a78bd3edcf95631f134
Rimantas <belauzas@yahoo.com>
2021-06-30 20:03:56 +0300

    Sticky animations

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 const backToTopDOM = document.querySelector('.back-to-top');
+const headerDOM = document.querySelector('header');
+let wasStickyAtLeastOne = false;
+let notStickyTimer = null;
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 500) {
         backToTopDOM.classList.remove('hide');
     } else {
         backToTopDOM.classList.add('hide');
+    }
+
+    if (window.scrollY > 150) {
+        wasStickyAtLeastOne = true;
+        headerDOM.classList.add('sticky');
+        headerDOM.classList.remove('not-sticky');
+    } else {
+        headerDOM.classList.remove('sticky');
+        if (wasStickyAtLeastOne) {
+            headerDOM.classList.add('not-sticky');
+
+            notStickyTimer = setTimeout(() => {
+                headerDOM.classList.remove('not-sticky');
+                wasStickyAtLeastOne = false;
+            }, 2000)
+        }
     }
 })
